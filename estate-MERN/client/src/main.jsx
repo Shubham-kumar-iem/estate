f37396bded0c4d2e3f432701468f46256e2971f6
Shubham Kumar <shubhammuz110203@gmail.com>
2024-03-14 23:53:10 +0530

    adding frontend URL to the main

--- estate-MERN/client/src/main.jsx
+++ estate-MERN/client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-dl33fbge1v4bf0xj.us.auth0.com"
       clientId="sedxHimtFQMiS8cvWBGyDRI5GuXrvUCq"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173"
+        redirect_uri: "https://gharlelo.vercel.app"
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
